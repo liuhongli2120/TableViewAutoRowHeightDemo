bff9c49f5de5c2447eb8ade211266674ada06ae3
刘宏立 <liuhongli2120@163.com>
2016-11-20 20:43:29 +0800

    创建自定义cell文件

---
+++ TableViewAutoRowHeightDemo/TableViewAutoRowHeightDemo/HLAutoHeightCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HLAutoHeightCellTableViewCell.h
+//  TableViewAutoRowHeightDemo
+//
+//  Created by 刘宏立 on 2016/11/20.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLAutoHeightCellTableViewCell : UITableViewCell
+
+@end
---
+++ TableViewAutoRowHeightDemo/TableViewAutoRowHeightDemo/HLAutoHeightCellTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  HLAutoHeightCellTableViewCell.m
+//  TableViewAutoRowHeightDemo
+//
+//  Created by 刘宏立 on 2016/11/20.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import "HLAutoHeightCellTableViewCell.h"
+
+@implementation HLAutoHeightCellTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+
+@end
